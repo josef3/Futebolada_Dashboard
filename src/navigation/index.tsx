a269d822caadad9073b5bb11a034708919b1b32f
josef3 <josefigueiredo12@gmail.com>
2021-12-03 10:52:31 +0000

    feat: :bug: add missing sidebar context

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -11,10 +11,11 @@ import WeeksNavigation from './weeks';
 import MvpsNavigation from './mvps';
 import BestFivesNavigation from './bestFives';
 import MvpVotesNavigation from './mvpVotes';
-// import BestFiveVotesNavigation from './bestFiveVotes';
+import BestFiveVotesNavigation from './bestFiveVotes';
 import Login from '../pages/Login';
 //----------------------------------------------------------
 import { AuthContextProvider } from '../contexts/auth';
+import { SidebarConfigContextProvider } from '../contexts/sidebarConfig';
 import ThemeConfig from '../theme';
 //----------------------------------------------------------
 
@@ -26,32 +27,34 @@ const Navigation: React.FC = () => {
         <Router>
             <Switch>
                 <AuthContextProvider>
-                    <ThemeConfig>
-                        <SnackbarProvider
-                            maxSnack={3}
-                            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-                            ref={notistackRef}
-                            action={(key) => (<IconButton onClick={onClickDismiss(key)}><CloseIcon /></IconButton>)}
-                        >
+                    <SidebarConfigContextProvider>
+                        <ThemeConfig>
+                            <SnackbarProvider
+                                maxSnack={3}
+                                anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                ref={notistackRef}
+                                action={(key) => (<IconButton onClick={onClickDismiss(key)}><CloseIcon /></IconButton>)}
+                            >
 
-                            <PlayersNavigation />
-                            <WeeksNavigation />
-                            <StatsNavigation />
-                            <MvpsNavigation />
-                            <BestFivesNavigation />
-                            <MvpVotesNavigation />
-                            {/* <BestFiveVotesNavigation /> */}
+                                <PlayersNavigation />
+                                <WeeksNavigation />
+                                <StatsNavigation />
+                                <MvpsNavigation />
+                                <BestFivesNavigation />
+                                <MvpVotesNavigation />
+                                <BestFiveVotesNavigation />
 
-                            <Route path='/login' exact>
-                                <Login />
-                            </Route>
+                                <Route path='/login' exact>
+                                    <Login />
+                                </Route>
 
-                            <Route path='/' exact>
-                                <Redirect to='/login' />
-                            </Route>
+                                <Route path='/' exact>
+                                    <Redirect to='/login' />
+                                </Route>
 
-                        </SnackbarProvider>
-                    </ThemeConfig>
+                            </SnackbarProvider>
+                        </ThemeConfig>
+                    </SidebarConfigContextProvider>
                 </AuthContextProvider>
             </Switch>
         </Router>
