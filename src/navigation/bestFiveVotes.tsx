1229a63003af5a2a500344b85fac94bea2b90a2a
josef3 <josefigueiredo12@gmail.com>
2021-12-03 10:57:12 +0000

    feat: :sparkles: add bestFiveVotes navigation

---
+++ src/navigation/bestFiveVotes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+//-------------------- Components --------------------------
+import Container from './Container';
+import Content from './Content';
+import PrivateRoute from '../components/PrivateRoute';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+import BestFiveVotes from '../pages/BestFiveVotes';
+//----------------------------------------------------------
+
+const BASE_URL = '/dashboard/votos/melhores-5';
+
+const BestFivesNavigation: React.FC = () => {
+    return (
+        <>
+            <PrivateRoute path={`${BASE_URL}`} exact>
+                <Container>
+                    <Sidebar />
+                    <Content>
+                        <Header />
+                        <BestFiveVotes />
+                    </Content>
+                </Container>
+            </PrivateRoute>
+        </>
+    )
+}
+
+export default BestFivesNavigation;
